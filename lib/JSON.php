1852dc4162fa4117431e60b4b503ef37625dca55
JacobNeergaard <neergaard@kananga.dk>
2026-08-24 09:12:50 +0200

    never-returning

--- lib/JSON.php
+++ lib/JSON.php
@@ -33,7 +33,7 @@ class JSON extends CurlOpt implements Data {
 		return json_decode($json, flags: JSON_THROW_ON_ERROR);
 	}
 
-	public static function output($value,int $code = 200) : void {
+	public static function output($value,int $code = 200) : never {
 		http_response_code($code);
 		if(!headers_sent()) {
 			header('Content-Type: application/json');
@@ -41,7 +41,7 @@ class JSON extends CurlOpt implements Data {
 		echo json_encode($value);
 		exit;
 	}
-	public static function error(string $string,int $code = 400) : void {
+	public static function error(string $string,int $code = 400) : never {
 		$error = ['code' => $code,'message' => $string];
 		self::output($error,$code);
 	}
